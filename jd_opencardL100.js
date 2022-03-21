a9e40ec04dbdb17042df8969e5d7b78c44f537e9
KingRan <kingran888@gmail.com>
2022-03-21 23:44:45 +0800

    更新

--- jd_opencardL100.js
+++ jd_opencardL100.js
@@ -225,8 +225,8 @@ async function run() {
       console.log(`后面的号都会助力:${$.shareUuid}`)
     }
     await $.wait(parseInt(Math.random() * 2000 + 5000, 10))
-      if($.index % 3 == 0) console.log('休息1分钟，别被黑ip了\n可持续发展')
-      if($.index % 3 == 0) await $.wait(parseInt(Math.random() * 5000 + 60000, 10))
+      if($.index % 3 == 0) console.log('休息一下，别被黑ip了\n可持续发展')
+      if($.index % 3 == 0) await $.wait(parseInt(Math.random() * 5000 + 30000, 10))
   } catch (e) {
     console.log(e)
   }
