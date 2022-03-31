c55c4e53c39451f38e671a4e9557aa7e366daf22
KingRan <kingran888@gmail.com>
2022-03-31 19:02:42 +0800

    更新

--- jd_cfd_help.js
+++ jd_cfd_help.js
@@ -100,7 +100,7 @@ if ($.isNode()) {
 			}
     }
   }
-  let res = await getAuthorShareCode('https://gitee.com/KingRan521/JD-Scripts/raw/master/shareCodes/cfd.json')
+  let res = await getAuthorShareCode('https://cdn.jsdelivr.net/gh/KingRan/shareCodes@master/cfd.json')
   $.strMyShareIds = [...(res || [])]
   await shareCodesFormat()
   for (let i = 0; i < cookiesArr.length; i++) {
