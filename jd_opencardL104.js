e94daa6d09b721e7d3d80025dbd3bbf3725d063a
KingRan <kingran888@gmail.com>
2022-03-31 14:05:50 +0800

    更新

--- jd_opencardL104.js
+++ jd_opencardL104.js
@@ -51,7 +51,7 @@ let activityCookie =''
   $.activityId = "dz721d61fe736531344021e810179d"
   $.shareUuid = "75cd279ce4f64026863bcb7bd613b862"
   console.log(`【入口:\nhttps://lzdz1-isv.isvjcloud.com/dingzhi/yapei/redenvelope/activity/activity?activityId=${$.activityId}&shareUuid=${$.shareUuid}】`)
-  let shareUuidArr = ["75cd279ce4f64026863bcb7bd613b862","902724ae33594b80afc7cb0567d080c9","7cadcef2d8d94b88a2803c8c0e5dd34c","ef70077cbb134017b327394e5cd280cf"]
+  let shareUuidArr = ["902724ae33594b80afc7cb0567d080c9","7cadcef2d8d94b88a2803c8c0e5dd34c","ef70077cbb134017b327394e5cd280cf"]
   let s = Math.floor((Math.random()*4))
   let n = 0
   n = Math.floor((Math.random()*shareUuidArr.length))
