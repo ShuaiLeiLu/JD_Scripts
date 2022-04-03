d5602316ef4c1c1efcfabac950b3c61bebc5a3cd
KingRan <kingran888@gmail.com>
2022-04-03 10:15:16 +0800

    更新

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -41,6 +41,7 @@ let roundList = [];
 let awardState = '';//上期活动的京豆是否收取
 let randomCount = $.isNode() ? 20 : 5;
 let num;
+let llerror=false;
 $.newShareCode = [];
 let NowHour = new Date().getHours();
 let lnrun = 0;
@@ -93,33 +94,8 @@ async function jdPlantBean() {
   try {
     console.log(`获取任务及基本信息`)
     await plantBeanIndex();
-    if ($.plantBeanIndexResult.errorCode === 'PB101') {
-        console.log(`\n活动太火爆了，还是去买买买吧！\n`)
-        return
-    }
-    if ($.plantBeanIndexResult.errorCode) {
-        console.log(`获取任务及基本信息出错，10秒后重试\n`)
-        await $.wait(10000);
-        await plantBeanIndex();
-        if ($.plantBeanIndexResult.errorCode === 'PB101') {
-            console.log(`\n活动太火爆了，还是去买买买吧！\n`)
-            return
-        }
-    }
-	if ($.plantBeanIndexResult.errorCode) {
-        console.log(`获取任务及基本信息出错，30秒后重试\n`)
-        await $.wait(30000);
-        await plantBeanIndex();
-        if ($.plantBeanIndexResult.errorCode === 'PB101') {
-            console.log(`\n活动太火爆了，还是去买买买吧！\n`)
-            return
-        }
-    }
-	if ($.plantBeanIndexResult.errorCode) {
-		console.log(`获取任务及基本信息失败，活动异常，换个时间再试试吧....`)
-		console.log("错误代码;"+$.plantBeanIndexResult.errorCode)
-		return
-	}
+    if(llerror)
+		return;
     for (let i = 0; i < $.plantBeanIndexResult.data.roundList.length; i++) {
       if ($.plantBeanIndexResult.data.roundList[i].roundState === "2") {
         num = i
@@ -199,6 +175,8 @@ async function doGetReward() {
 }
 async function doCultureBean() {
   await plantBeanIndex();
+	if(llerror)
+		return;
   if ($.plantBeanIndexResult && $.plantBeanIndexResult.code === '0') {
     const plantBeanRound = $.plantBeanIndexResult.data.roundList[num]
     if (plantBeanRound.roundState === '2') {
@@ -429,6 +407,8 @@ async function doTask() {
 function showTaskProcess() {
   return new Promise(async resolve => {
     await plantBeanIndex();
+	if(llerror)
+		return;
     $.taskList = $.plantBeanIndexResult.data.taskList;
     if ($.taskList && $.taskList.length > 0) {
       console.log("     任务   进度");
@@ -548,7 +528,39 @@ async function helpShare(plantUuid) {
   console.log(`助力结果的code:${$.helpResult && $.helpResult.code}`);
 }
 async function plantBeanIndex() {
-  $.plantBeanIndexResult = await request('plantBeanIndex');//plantBeanIndexBody
+	llerror=false;
+    $.plantBeanIndexResult = await request('plantBeanIndex'); //plantBeanIndexBody
+    if ($.plantBeanIndexResult.errorCode === 'PB101') {
+        console.log(`\n活动太火爆了，还是去买买买吧！\n`)
+		llerror=true;
+        return
+    }
+    if ($.plantBeanIndexResult.errorCode) {
+        console.log(`获取任务及基本信息出错，10秒后重试\n`)
+        await $.wait(10000);
+        $.plantBeanIndexResult = await request('plantBeanIndex'); 
+        if ($.plantBeanIndexResult.errorCode === 'PB101') {
+            console.log(`\n活动太火爆了，还是去买买买吧！\n`)
+			llerror=true;
+            return
+        }
+    }
+    if ($.plantBeanIndexResult.errorCode) {
+        console.log(`获取任务及基本信息出错，30秒后重试\n`)
+        await $.wait(30000);
+        $.plantBeanIndexResult = await request('plantBeanIndex'); 
+        if ($.plantBeanIndexResult.errorCode === 'PB101') {
+            console.log(`\n活动太火爆了，还是去买买买吧！\n`)
+			llerror=true;
+            return
+        }
+    }
+    if ($.plantBeanIndexResult.errorCode) {
+        console.log(`获取任务及基本信息失败，活动异常，换个时间再试试吧....`)
+        console.log("错误代码;" + $.plantBeanIndexResult.errorCode)
+		llerror=true;
+        return
+    }
 }
 function requireConfig() {
   return new Promise(resolve => {
