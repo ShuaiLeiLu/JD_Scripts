9fed5e9ad8f5aef3a51bf05c858be28cbcb3d7fc
KingRan <kingran888@gmail.com>
2022-04-11 01:08:54 +0800

    自己抓log

--- backUp/jinli_log.js
+++ backUp/jinli_log.js
@@ -4,6 +4,16 @@ let logs = [
 
 ]
 
-export {
-  logs
+
+if (process.env.logs) {
+    if (process.env.logs.indexOf('\n') > -1) {
+
+        logs = process.env.logs.split('\n');
+    } else {
+        logs = process.env.logs.split();
+    }
+}
+for (let i = 0; i < logs.length; i++) {
+    const index = (i + 1 === 1) ? '' : (i + 1);
+    exports['logs' + index] = logs[i];
 }
