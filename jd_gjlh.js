d38e5726543764bc53cab47b46a28733f2415103
KingRan <kingran888@gmail.com>
2022-04-11 00:41:08 +0800

    更新

--- jd_gjlh.js
+++ jd_gjlh.js
@@ -21,7 +21,7 @@ if ($.isNode()) {
     cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
 }
 !(async () => {
-	console.log('入口下拉：https://prodev.m.jd.com/mall/active/ymjgzZHsgzyCr2zoDcg7wrgEZLK/index.html')
+	console.log('入口下拉：https://prodev.m.jd.com/mall/active/48Ki4bRbQFE4izpBis95dxZJSsTq/index.html')
     if (!cookiesArr[0]) {
         $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
         return;
