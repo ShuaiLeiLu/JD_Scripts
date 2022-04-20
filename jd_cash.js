02d93a3377e53d68c60bb266925da02a7468481f
KingRan <kingran888@gmail.com>
2022-04-20 13:55:40 +0800

    更新

--- jd_cash.js
+++ jd_cash.js
@@ -283,7 +283,7 @@ function getSignfromPanda(functionId, body) {
     }
     return new Promise((resolve) => {
         let url = {
-            url: "https://api.jds.codes/jd/sign",
+            url: "https://api.zhezhe.cf/jd/sign",
             body: JSON.stringify(data),
 		    followRedirect: false,
 		    headers: {
