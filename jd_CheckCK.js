db9bbf33629c446a28cbef6fe9ec91d9203f2692
KingRan <kingran888@gmail.com>
2022-04-28 23:52:32 +0800

    更新

--- jd_CheckCK.js
+++ jd_CheckCK.js
@@ -1,5 +1,5 @@
 /*
-cron "30 * * * *" jd_CheckCK.js, tag:京东CK检测by-ccwav
+cron "30 2-22/2 * * *" jd_CheckCK.js, tag:京东CK检测by-ccwav
  */
 //详细说明参考 https://github.com/ccwav/QLScript2.
 const $ = new Env('京东CK检测');
