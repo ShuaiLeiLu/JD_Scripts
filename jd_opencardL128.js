eed22902c4a637d2d0a34eaf37de01b66c82d0aa
KingRan <kingran888@gmail.com>
2022-05-01 02:44:26 +0800

    更新

--- jd_opencardL128.js
+++ jd_opencardL128.js
@@ -517,7 +517,7 @@ async function dealReturn(type, data) {
               }else if(type == 'toMainActive'){
                 title = '逛会场'
               }else{
-                msg = res.data.drawOk == true && (res.data.drawInfoType == 6 && res.data.name || '') || res.data.wdsrvo.drawOk == true && (res.data.wdsrvo.drawInfoType == 6 && res.data.wdsrvo.name || '') || '空气💨'
+                msg = res.data.drawOk == true && (res.data.drawInfoType == 6 && res.data.name || '') || res.data.wdsrvonw.drawOk == true && (res.data.wdsrvo.drawInfoType == 6 && res.data.wdsrvo.name || '') || '空气💨'
               }
               if(!msg){
                 msg = '空气💨'
