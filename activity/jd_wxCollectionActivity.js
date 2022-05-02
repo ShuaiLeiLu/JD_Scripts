e9cfd80cf4f32db852082ae9669059a48200c025
KingRan <kingran888@gmail.com>
2022-05-02 12:51:53 +0800

    更新

--- activity/jd_wxCollectionActivity.js
+++ activity/jd_wxCollectionActivity.js
@@ -293,7 +293,7 @@ function getMyPing() {
 }
 function getFirstLZCK() {
     return new Promise(resolve => {
-        $.get({ url: $.activityUrl }, (err, resp, data) => {
+        $.get({ url: $.activityUrl ,headers:{"user-agent":$.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1")}}, (err, resp, data) => {
             try {
                 if (err) {
                     console.log(err)
