93e152d710df66b6b3cceb0382ff6b5a56464850
KingRan <kingran888@gmail.com>
2022-05-10 20:04:39 +0800

    更新

--- jd_zjd.ts
+++ jd_zjd.ts
@@ -7,7 +7,7 @@
  */
 
 import axios from "axios";
-import {zjdInit, zjdH5st} from "./function/jd_zjd_tool.js";
+import {zjdInit, zjdH5st} from "./function/zjd_tool.js";
 import {o2s, wait, requireConfig} from "./function/TS_USER_AGENTS";
 import {SHA256} from "crypto-js";
 
