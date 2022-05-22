a4e4b082f741c0944d070e8487b30328b61acd31
KingRan <kingran888@gmail.com>
2022-05-22 13:10:31 +0800

    更新

--- jd_opencardL151.js
+++ jd_opencardL151.js
@@ -610,8 +610,8 @@ async function dealReturn(type, data) {
             console.log('已经助力过')
           }else if(res.data.status == 104){
             console.log('已经助力其他人')
-          }else if(res.data.status == 101){
-            // console.log('已经助力过')
+          }else if(res.data.status == 103){
+            console.log('不是所有店铺新会员，无法助力')
           }else{
             console.log(data)
           }
