67e804ace4e668cd2fede578c5d74fbbf0d7c054
KingRan <kingran888@gmail.com>
2022-05-27 00:46:12 +0800

    更新

--- jd_joypark_run.ts
+++ jd_joypark_run.ts
@@ -2,8 +2,8 @@
 汪汪乐园-跑步+组队
 默认翻倍到0.04红包结束,修改请设置变量
 export JD_JOY_PARK_RUN_ASSETS="0.04"
-cron:20 * * * *
-20 * * * * jd_joy_park_run.ts
+cron:30 0 * * * *
+30 0 * * * * jd_joy_park_run.ts
 new Env('极速版汪汪赛跑');
 
 **/
