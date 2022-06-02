8bd08d48e2666795b00e1dbe3e1ef0902edf07b1
KingRan <kingran888@gmail.com>
2022-06-02 15:19:00 +0800

    更新

--- jd_618red.js
+++ jd_618red.js
@@ -30,7 +30,13 @@ let appId, fingerprint, token, enCryptMethodJD;
         $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
         return;
     }
-    console.log('请务必填写你的Code！变量：export CODE618="" \n\n请务必填写你的Code！变量：export CODE618=""\n\n个人建议禁用,避免其他问题\n')
+		if (!$.CODE618 && cookiesArr.length > 15){
+			console.log(`\n衰仔、你的账号数量已经超越大部分人啦！o(╥﹏╥)o\n\n不填写准确的变量实在无法运行！[○･｀Д´･ ○]\n\n变量格式▶▶▶：export CODE618=""\n`)
+			return;
+		}
+		if (cookiesArr.length <= 15){
+			console.log(`\n推荐填写自己的Code！o(╥﹏╥)o\n变量格式▶▶▶：export CODE618=""\n`)
+		} 		
     $.CryptoJS = $.isNode() ? require('crypto-js') : CryptoJS;
     appId = '6a98d';
     let fglist = ['6289931560897925', '0403403318679778', '1390288884563462'];
@@ -44,6 +50,7 @@ let appId, fingerprint, token, enCryptMethodJD;
     let runCK = [];
     for (let i = 0; i < cookiesArr.length; i += 1) {
         runCK.push(cookiesArr.slice(i, i + 1));
+				
     }
     for (let i = 0; i < runCK.length; i++) {
         const promiseArr = runCK[i].map((ck, index) => main(ck));
