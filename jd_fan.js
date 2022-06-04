aa5de70c74e60367b33b8e42e81ce09c690546d9
KingRan <kingran888@gmail.com>
2022-06-04 08:59:56 +0800

    更新

--- jd_fan.js
+++ jd_fan.js
@@ -24,6 +24,8 @@ if ($.isNode()) {
 }
 let activityList = [
     {'id':'b4a30e4b5b3c4a07aa879afd8e9de3f0','endTime':1656626274000},//
+		{'id':'c88ffa4a17b04cc994a6e66f161471','endTime':1656626274000},//
+		
 ];
 !(async()=>{
 	activityList=getRandomArrayElements(activityList,activityList.length);
