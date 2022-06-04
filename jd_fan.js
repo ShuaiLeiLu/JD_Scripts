a46e4b77cde5f8ef60eaf0dd18e8e87f21a9d2a7
KingRan <kingran888@gmail.com>
2022-06-05 00:10:33 +0800

    更新

--- jd_fan.js
+++ jd_fan.js
@@ -23,7 +23,7 @@ if ($.isNode()) {
         ...$.toObj($.getdata("CookiesJD") || "[]").map((item) => item.cookie)].filter((item) => !!item);
 }
 let activityList = [
-    {'id':'b4a30e4b5b3c4a07aa879afd8e9de3f0','endTime':1656626274000},//
+    {'id':'cebd38c28cf0468c8082082eebde4c32','endTime':1656626274000},//
 		{'id':'c88ffa4a17b04cc994a6e66f161471','endTime':1656626274000},//
 		
 ];
