4a747ebe3d76fb17c1d87a0693f771bd917e0465
KingRan <kingran888@gmail.com>
2022-07-09 15:13:21 +0800

    更新

--- jd_joy_run_reward.ts
+++ jd_joy_run_reward.ts
@@ -2,7 +2,8 @@
 汪汪赛跑-提现10元
 2 0 0 * * 5 jd_joy_run_reward.ts
 new Env('汪汪赛跑提现')
-
+Modify By Dylan from HW
+updateTime：2022-07-09
 **/
 
 import {get, post, requireConfig, wait} from './function/TS_USER_AGENTS'
