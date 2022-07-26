84a2ebf59167246fde53e75bff5661e8bfa78bf6
LSL <9626471@qq.com>
2022-07-26 11:19:11 +0800

    报错修改

--- utils/jd_get_share_code.js
+++ utils/jd_get_share_code.js
@@ -450,7 +450,7 @@ async function getPlantBean() {
     async function jdPlantBean() {
         await plantBeanIndex();
         // console.log(plantBeanIndexResult.data.taskList);
-        if ($.plantBeanIndexResult.code === "0" && $.plantBeanIndexResult.data.jwordShareInfo) {
+        if ($.plantBeanIndexResult.code === "0" && $.plantBeanIndexResult.data && $.plantBeanIndexResult.data.jwordShareInfo) {
             const shareUrl = $.plantBeanIndexResult.data.jwordShareInfo.shareUrl;
             $.myPlantUuid = getParam(shareUrl, "plantUuid");
             console.log(`【京东账号${$.index}（${$.UserName}）种豆得豆】${$.myPlantUuid}`);
