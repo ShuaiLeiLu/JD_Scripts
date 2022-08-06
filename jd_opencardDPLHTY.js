3972a0e3c9d7116445a7e4b0d91066d9323f9640
KingRan <kingran888@gmail.com>
2022-08-06 11:19:02 +0800

    更新

--- jd_opencardDPLHTY.js
+++ jd_opencardDPLHTY.js
@@ -52,6 +52,8 @@ if ($.isNode()) {
 } else {
   cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
 }
+let opencard_draw = "0"
+opencard_draw = $.isNode() ? (process.env.opencard_draw ? process.env.opencard_draw : opencard_draw) : ($.getdata('opencard_draw') ? $.getdata('opencard_draw') : opencard_draw);
 opencard_toShop = $.isNode() ? (process.env.opencard_toShop ? process.env.opencard_toShop : `${opencard_toShop}`) : ($.getdata('opencard_toShop') ? $.getdata('opencard_toShop') : `${opencard_toShop}`);
 
 allMessage = ""
