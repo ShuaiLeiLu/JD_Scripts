eff2119c80c9aadaf9cf1e1dd247ee4b55d815d9
LSL <9626471@qq.com>
2022-10-25 15:43:40 +0800

    穿行空间

--- jd_rabbit_racx.py
+++ jd_rabbit_racx.py
@@ -4,15 +4,23 @@ new Env('热爱穿行-2022');
 export RabbitToken="token值"
 export CXJ_THREADS_NUMS=1
 export CXJ_PINS="pin1,pin2,pin3"
+export CXJ_NUM="0"
+export CXJ_RAISE="1"
 
 
 CXJ_THREADS_NUMS: 并发数
 CXJ_PIN: 需要跑的ck的pin值
+CXJ_NUM: 需要的前多少个号为车头
+CXJ_RAISE: 是否自动升级，1：为自动升级，0为不自动升级
 
 log剩余次数大于5000方可使用
 '''
 
 import os
+
+if not os.environ.get("LogHost"):
+    print("请先填写LogHost变量\nexport LogHost='xxxxx'")
+    exit()
 
 try:
     CXJ_THREADS_NUM = int(os.environ.get("CXJ_THREADS_NUMS", 1))
--- jd_rabbit_racx_zd.py
+++ jd_rabbit_racx_zd.py
@@ -2,14 +2,16 @@
 new Env('热爱穿行-组队-2022');
 export RabbitToken="token值"
 export ZD_ReverseCk=0 或 1 或 2
-export RABBIT_HELP_PIN="pin1,pin2,pin3"
-export RABBIT_HELP_NUM=200
+export ZD_HELP_PIN="pin1,pin2,pin3"
+export ZD_HELP_NUM=200
+export ZD_GROUP_MAX_NUM=30
 
 变量:
 RabbitToken： 机器人给你发的token
-RABBIT_HELP_PIN：设置车头pin
-RABBIT_HELP_NUM：设置前多少个号为车头,设置了pin时，这个设置失效
+ZD_HELP_PIN：设置车头pin
+ZD_HELP_NUM：设置前多少个号为车头,设置了pin时，这个设置失效
 ZD_ReverseCk：0：正序，1：反序，2：乱序
+ZD_GROUP_MAX_NUM：每个队伍的人数
 
 log剩余次数大于5000方可使用
 '''
