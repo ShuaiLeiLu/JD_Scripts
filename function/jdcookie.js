746131e3ab58b4c51b39780ccd811d78cbc3f893
LSL <9626471@qq.com>
2022-12-01 11:13:20 +0800

    自动化种植

--- function/jdcookie.js
+++ function/jdcookie.js
@@ -32,4 +32,4 @@ for (let i = 0; i < CookieJDs.length; i++) {
     const index = (i + 1 === 1) ? '' : (i + 1);
     exports['CookieJD' + index] = CookieJDs[i].trim();
 }
-console.log('>>>>>>>>>>>>>>6Dylan6 提示：任务正常运行中>>>>>>>>>>>>>>>\n')
+console.log('>>>>>>>>>>>>>>风流 提示：任务正常运行中>>>>>>>>>>>>>>>\n')
