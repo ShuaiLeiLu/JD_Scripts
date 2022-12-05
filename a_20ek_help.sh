9fb73450ad02fedf9e7d460aef3d7fb3bd7a5b04
LSL <9626471@qq.com>
2022-12-05 10:43:20 +0800

    bbk 赚钱大赢家

---
+++ a_20ek_help.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# new Env('BBK-20E卡助力');
+# export JD_LOG_XYZ_TOKEN="从机器人获取的token"
+# export Proxy_Url="代理网址 例如：星空、熊猫 生成选择txt 一次一个"
+# export ECARD20_DELAY="2" #每个ck助力后等待2秒
+# export ECARD20_CK_START_INDEX="10" #从第10个号开始助力 可选参数,可以不填。
+# export ECARD20_NUM="30" #助力人数，默认30人 可选参数,可以不填。
+# 需要20ek_list.txt，一行一个助力链接
+pwd
+_ftype=""
+get_arch=`arch`
+use_get_arch=${BBK_ARCH}
+if [ "$use_get_arch" != "" ]; then
+  get_arch=$use_get_arch
+  echo "指定运行$use_get_arch"
+fi
+echo $get_arch
+if [[ $get_arch =~ "x86_64" ]];then
+	_ftype="linux-amd64"
+elif [[ $get_arch =~ "x86" ]];then
+	_ftype="linux-386"
+elif [[ $get_arch =~ "i386" ]];then
+	_ftype="linux-386"
+elif [[ $get_arch =~ "aarch64" ]];then
+	_ftype="linux-arm64"
+elif [[ $get_arch =~ "arm" ]];then
+	_ftype="linux-arm"
+else
+	_ftype=""
+fi
+
+if [ $_ftype == "" ]; then
+	echo "不支持的架构$get_arch"
+else
+	echo "执行$_ftype"
+    if [ -f "$PWD/BBK/$_ftype.bbk" ]; then
+        echo "$PWD/BBK/$_ftype.bbk"
+        eval "chmod +x ./BBK/$_ftype.bbk"
+        eval "./BBK/$_ftype.bbk -t 20ek_help"
+    else
+        if [ ! -f "$PWD/$_ftype.bbk" ]; then
+            echo "在$PWD/BBK目录、$PWD目录下均未找到文件$_ftype.bbk"
+            exit 1
+        fi
+        echo "$PWD/$_ftype.bbk"
+        eval "chmod +x $PWD/$_ftype.bbk"
+        eval "$PWD/$_ftype.bbk -t 20ek_help"
+    fi
+fi
