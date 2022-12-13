f5251a5e69bf9cb4c7905cad53fada724cf50bb6
LSL <9626471@qq.com>
2022-12-13 19:05:27 +0800

    bbk 脚本更新

---
+++ a_dd_help.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# new Env('BBK-点点券助力');
+# export JD_LOG_XYZ_TOKEN="从机器人获取的token"
+# export Proxy_Url="代理网址 例如：星空、熊猫 生成选择txt 一次一个"
+# export DD_HELP_PINS="jd_xxx&jd_xxx" #助力指定pin
+# export DD_DELAY="2" # 助力等待多少秒
+# export DD_HELP_STARTCK="10" #助力前10个ck，从第11个ck开始助力 可选参数
+# export DD_USE_PROXY="true" #强制使用代理
+pwd
+_ftype=""
+get_arch=`arch`
+use_get_arch=${BBK_ARCH}
+if [ "$use_get_arch" != "" ]; then
+  get_arch=$use_get_arch
+  echo "指定运行$use_get_arch"
+fi
+echo $get_arch
+if [[ $get_arch =~ "x86_64" ]];then
+	_ftype="linux-amd64"
+elif [[ $get_arch =~ "x86" ]];then
+	_ftype="linux-386"
+elif [[ $get_arch =~ "i386" ]];then
+	_ftype="linux-386"
+elif [[ $get_arch =~ "aarch64" ]];then
+	_ftype="linux-arm64"
+elif [[ $get_arch =~ "arm" ]];then
+	_ftype="linux-arm"
+else
+	_ftype=""
+fi
+
+if [ $_ftype == "" ]; then
+	echo "不支持的架构$get_arch"
+else
+	echo "执行$_ftype"
+    if [ -f "$PWD/BBK/$_ftype.bbk" ]; then
+        echo "$PWD/BBK/$_ftype.bbk"
+        eval "chmod +x ./BBK/$_ftype.bbk"
+        eval "./BBK/$_ftype.bbk -t dd_help"
+    else
+        if [ ! -f "$PWD/$_ftype.bbk" ]; then
+            echo "在$PWD/BBK目录、$PWD目录下均未找到文件$_ftype.bbk"
+            exit 1
+        fi
+        echo "$PWD/$_ftype.bbk"
+        eval "chmod +x $PWD/$_ftype.bbk"
+        eval "$PWD/$_ftype.bbk -t dd_help"
+    fi
+fi
