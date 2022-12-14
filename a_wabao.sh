f6b04c41b53cbb915de4a97579528f5f3e1c309f
LSL <9626471@qq.com>
2022-12-14 15:05:30 +0800

    定时修改

--- a_wabao.sh
+++ a_wabao.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-# new Env('BBK-20E卡助力');
+# new Env('BBK-挖宝顺序助力版');
 # export JD_LOG_XYZ_TOKEN="从机器人获取的token"
 # export Proxy_Url="代理网址 例如：星空、熊猫 生成选择txt 一次一个"
-# export ECARD20_DELAY="2" #每个ck助力后等待2秒
-# export ECARD20_CK_START_INDEX="10" #从第10个号开始助力 可选参数,可以不填。
-# export ECARD20_NUM="30" #助力人数，默认30人 可选参数,可以不填。
-# 需要20ek_list.txt，一行一个助力链接
+# export WABAO_SUSSCESS_COUNT="111" #挖宝助力次数限制，助力111次
+# export WABAO_PINS="指定pin助力，多个用英文&分割"
+# export WABAO_BAN_PINS="123&456" #挖宝助力时这里指定的pin将不助力(黑名单)
+# export CK_START_INDEX="10"  #从第10个号开始助力 可选参数,可以不填。
+# export WABAO_SHUNXU_DELAY="2" # 助力等待多少秒 默认0秒 可选参数,可以不填。
+# export WABAO_USE_PROXY="true" #强制使用代理 默认不用代理 可选参数,可以不填。
+# 支持PIN或者链接方式(`wabaolist.txt`存放需要助力的URL,一行一个)
 pwd
 _ftype=""
-get_arch=`arch`
 use_get_arch=${BBK_ARCH}
+get_arch=`arch`
 if [ "$use_get_arch" != "" ]; then
   get_arch=$use_get_arch
   echo "指定运行$use_get_arch"
@@ -36,7 +39,7 @@ else
     if [ -f "$PWD/BBK/$_ftype.bbk" ]; then
         echo "$PWD/BBK/$_ftype.bbk"
         eval "chmod +x ./BBK/$_ftype.bbk"
-        eval "./BBK/$_ftype.bbk -t 20ek_help"
+        eval "./BBK/$_ftype.bbk -t wabao"
     else
         if [ ! -f "$PWD/$_ftype.bbk" ]; then
             echo "在$PWD/BBK目录、$PWD目录下均未找到文件$_ftype.bbk"
@@ -44,6 +47,6 @@ else
         fi
         echo "$PWD/$_ftype.bbk"
         eval "chmod +x $PWD/$_ftype.bbk"
-        eval "$PWD/$_ftype.bbk -t 20ek_help"
+        eval "$PWD/$_ftype.bbk -t wabao"
     fi
 fi
